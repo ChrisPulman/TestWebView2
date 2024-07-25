8f63a2b331173da58d9ec2c02780c42fea667ae9
Dell <Dell@DESKTOP-4CHQ2HI>
2024-07-25 14:27:02 +0800

    添加项目文件。

---
+++ TestWebView2/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace TestWebView2;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private void App_OnStartup(object sender, StartupEventArgs e)
+    {
+        Application.Current.MainWindow = new MainWindow();
+        LoginWindow loginWindow = new LoginWindow();
+        if (loginWindow.ShowDialog() == true)
+        {
+            Application.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ TestWebView2/LoginWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+namespace TestWebView2;
+
+public partial class LoginWindow : Window
+{
+    public LoginWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        DialogResult = true;
+    }
+}
